92e6f4fc51c4129dfb38c8afaa640bd3863a7f6d
Nestor Salazar <salazarnestor1996@gmail.com>
2025-02-09 17:47:50 -0500

    Create the user validation schemas and user database model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,38 @@
+from enum import Enum
+
 from sqlmodel import Field, SQLModel
+from pydantic import EmailStr
 from datetime import datetime
 
 
+# Users
+class RoleEnum(str, Enum):
+    recruiter = "recruiter"
+    candidate = "candidate"
+
+
+class UserBase(SQLModel):
+    first_name: str = Field(min_length=3, max_length=50)
+    last_name: str = Field(min_length=3, max_length=50)
+    email: EmailStr = Field(min_length=3, max_length=50)
+    role: RoleEnum
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default=datetime.now(), nullable=False)
+    hashed_password: str = Field(nullable=False)
+
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=8, max_length=32)
+
+
+class UserPublic(UserBase):
+    id: int
+
+
+# Jobs
 class JobBase(SQLModel):
     title: str
     description: str
