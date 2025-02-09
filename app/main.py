eaaf9c5a82fc326bf37728e6e8b463b1d529343e
Nestor Salazar <salazarnestor1996@gmail.com>
2025-02-08 21:11:13 -0500

    Set up the database

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from .db import create_db_and_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    print("Startup: Database tables created")
+    yield
+    print("Shutdown: Application is closing")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
