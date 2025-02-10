14299e0fe4b5c49028c02cdcc1129d43314ff178
Nestor Salazar <salazarnestor1996@gmail.com>
2025-02-09 21:00:20 -0500

    Create the dependency to get the current user from the incoming JWT token

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,9 +1,14 @@
 from typing import Annotated
 
-from fastapi import Depends
-from sqlmodel import Session
+from fastapi import Depends, HTTPException, status
+from sqlmodel import Session, select
+from jwt.exceptions import InvalidTokenError
+import jwt
 
 from .db import engine
+from .core.security import oauth2_scheme
+from .core.config import settings
+from .models import User, TokenData
 
 
 def get_session():
@@ -12,3 +17,28 @@ def get_session():
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
+
+
+def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    session: SessionDep,
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(email=email)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = session.exec(select(User).where(User.email == token_data.email)).first()
+    if user is None:
+        raise credentials_exception
+    return user
